26e2660d40f3531056b0a391bb3e8e7e3c5bc51e
Dipayan Ghosh <122536428+dipayancodes@users.noreply.github.com>
2024-10-21 11:50:36 +0530

    Update sensex_lot_calculator.py

--- sensex_lot_calculator.py
+++ sensex_lot_calculator.py
@@ -1,3 +1,4 @@
+# SENSEX LOT CALCULATOR
 def calculate_quantity(amount, premium_price=100, lot_size=10):
     cost_per_lot = premium_price * lot_size
     number_of_lots = amount // cost_per_lot
@@ -10,3 +11,16 @@ amount = int(input("Enter your amount: "))
 quantity = calculate_quantity(amount)
 print(f"With ₹{amount}, you can trade {quantity} contracts of {quantity / lot_size} lots")
 
+# NIFTY LOT CALCULATOR
+
+def calculate_quantity(amount, premium_price=40, lot_size=25):
+    cost_per_lot = premium_price * lot_size
+    number_of_lots = amount // cost_per_lot
+    quantity = number_of_lots * lot_size
+    return quantity
+
+# Example usage
+lot_size = 25
+amount = int(input("Enter your amount: "))
+quantity = calculate_quantity(amount)
+print(f"With ₹{amount}, you can trade {quantity} contracts of {quantity / lot_size} lots")
